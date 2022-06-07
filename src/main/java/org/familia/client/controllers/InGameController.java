34ba8e8035df3496c6dc1d8729ab174f1fef325a
William <wiliamwijaya1985@gmail.com>
2022-06-08 01:12:22 +0700

    feat: add code to show in game methods

--- src/main/java/org/familia/client/controllers/InGameController.java
+++ src/main/java/org/familia/client/controllers/InGameController.java
@@ -12,18 +12,37 @@ public class InGameController extends Controller {
     public InGameController() throws Exception {
         layout = new InGameLayout(players, currPlayerIdx);
 
+//        new Thread(() -> {
+//            try {
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            layout.board.movePlayerTo(1, 2);
+//            try {
+//                layout.playerBox.next();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+
+//        layout.playerWin(0);
+//        layout.playerWin(1);
+//        layout.playerBox.next();
+
+//        layout.addOverlayToPane("dice");
+
+//        layout.addOverlayToPane("loading");
+
+//        layout.addOverlayToPane("networkError");
+
         new Thread(() -> {
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            layout.board.movePlayerTo(1, 2);
-            try {
-                layout.playerBox.next();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            layout.removeOverlayFromPane();
         }).start();
     }
 
